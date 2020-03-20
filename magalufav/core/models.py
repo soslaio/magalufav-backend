fcd9c77e28c47c9b8042001ba34ea474cd4979e6
Rogério Pereira <rogeriorp@gmail.com>
2020-03-20 08:32:22 -0300

    feat: hash dos clientes e rota detail

--- magalufav/core/models.py
+++ magalufav/core/models.py
@@ -1,10 +1,19 @@
 
+import time
+import hashlib
 from django.db import models
+
+
+def _create_hash():
+    m = hashlib.sha1()
+    m.update(str(time.time()).encode('utf-8'))
+    return m.hexdigest()
 
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=200)
     email = models.CharField(max_length=200, unique=True)
+    hash = models.CharField(max_length=200, default=_create_hash, unique=True)
 
     def __str__(self):
         return self.nome
@@ -13,6 +22,7 @@ class Cliente(models.Model):
 class Favorito(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     produto_id = models.IntegerField()
+    hash = models.CharField(max_length=200, default=_create_hash, unique=True)
 
     class Meta:
         unique_together = (('cliente', 'produto_id'),)
--- magalufav/core/views.py
+++ magalufav/core/views.py
@@ -9,16 +9,26 @@ from .serializers import FavoritoSerializer, ClienteSerializer
 
 class ClientesViewSet(viewsets.ViewSet):
 
-    def list(self, request):
-        queryset = Cliente.objects.all()
-        serializer = ClienteSerializer(queryset, many=True)
+    def retrieve(self, request, pk=None):
+        cliente = get_object_or_404(Cliente, hash=pk)
+        serializer = ClienteSerializer(cliente)
         return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        queryset = Cliente.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = ClienteSerializer(user)
-        return Response(serializer.data)
+    def create(self, request):
+        try:
+            serializer = ClienteSerializer(data=request.data)
+            validacao = serializer.is_valid(raise_exception=True)
+
+            return Response(status=201, data={'id': validacao})
+        except Exception as err:
+            errors = [dict(field=k, errors=v) for k, v in err.detail.items()]
+            return Response(status=400, data=dict(errors=errors))
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
 
 
 class FavoritosViewSet(viewsets.ViewSet):
