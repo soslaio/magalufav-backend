33a51e92dae33cc4127393c796037f4295dc6de6
Rogério Pereira <rogeriorp@gmail.com>
2020-03-23 18:38:47 -0300

    chore: ajuste do script de inicialização

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 sudo docker-compose -f stack.yml up -d --build
+echo "Aguardando inicialização dos containers..."
+sleep 15
+echo "Executando scripts de migração..."
 sudo docker exec -it magalu-api sh -c "python manage.py migrate"
 sudo docker exec -it magalu-api sh -c "echo \"from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@magazineluiza.com.br', 'admin_pass')\" | python manage.py shell"
+echo "Tudo pronto!"
